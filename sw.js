87e44813504a0ae64d3219a4cd744cfa895bc739
Sara-cho <kfcyk20@gmail.com>
2026-04-08 13:59:34 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+var CACHE = 'wdwave-v1';
+var FILES = ['/', '/index.html', '/consult_form.html', '/claim_guide.html', '/ecard.html'];
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }));
+});
+self.addEventListener('fetch', function(e){
+  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
+});
